26cd2f1478b9ebeb548bed318b9b920890c50752
Raihan Sharif <raihan@Raihans-MacBook-Pro.local>
2024-03-20 02:46:52 +0600

    Create order added

--- src/app/modules/orders/orders.controller.ts
+++ src/app/modules/orders/orders.controller.ts
@@ -3,15 +3,16 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { IOrders } from './orders.interface';
 import httpStatus from 'http-status';
+import { OrdersService } from './orders.service';
 // import { OrdersService } from './orders.service';
 
 const createOrders = catchAsync(async (req: Request, res: Response) => {
-  //   const result = await OrdersService.createOrder(req.body);
+  const result = await OrdersService.createOrder(req.body);
   sendResponse<IOrders>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Order created successfully!',
-    // data: result,
+    data: result,
   });
 });
 
--- src/app/modules/orders/orders.service.ts
+++ src/app/modules/orders/orders.service.ts
@@ -1,14 +1,67 @@
-// import httpStatus from 'http-status';
-// import ApiError from '../../../errors/apiError';
-// import { Cow } from '../cow/cow.model';
-// import User from '../user/user.model';
-// import { IOrders } from './orders.interface';
+import httpStatus from 'http-status';
+import { Cow } from '../cow/cow.model';
+import User from '../user/user.model';
+import ApiError from '../../../errors/apiError';
+import mongoose from 'mongoose';
+import { Orders } from './orders.model';
+import { IOrders } from './orders.interface';
 
-// const createOrder = async data => {
-//   //   const cowDetail = await Cow.findById(data.cow);
-//   //   const buyerDetail = await User.findById(data.buyer);
-// };
+const createOrder = async (data: IOrders): Promise<IOrders | null> => {
+  const cowDetail = await Cow.findById(data.cow);
+  const buyerDetail = await User.findById(data.buyer);
+  if (!cowDetail) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Cow not found');
+  }
+  if (!buyerDetail) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Buyer not found');
+  }
+
+  if (cowDetail.price > buyerDetail.budget) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Insufficient budget');
+  }
+  if (cowDetail.label == 'sold out') {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Cow is not for sell');
+  }
+  if (buyerDetail.role == 'seller') {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Buyer is not a buyer');
+  }
+  let newOrderAllData = null;
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    // Update the cow's label to 'sold out'
+    await Cow.updateOne({ _id: cowDetail.id }, { $set: { label: 'sold out' } });
+    // Deduct the cost of the cow from the buyer's budget
+    await User.updateOne(
+      { _id: buyerDetail.id },
+      { $set: { budget: buyerDetail.budget - cowDetail.price } },
+    );
+    //Add the cost of the cow to the seller income
+    const sellerData = await User.findOne({ _id: cowDetail.seller });
+    if (sellerData) {
+      const sellerUpdate = {
+        income: sellerData?.income + cowDetail.price,
+      };
+      await User.updateOne({ _id: sellerData.id }, sellerUpdate);
+    }
+
+    const newOrder = await Orders.create([data], { session });
+
+    if (!(await newOrder).length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create order');
+    }
+    newOrderAllData = newOrder[0];
+
+    await session.commitTransaction();
+    await session.endSession();
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+  }
+  return newOrderAllData;
+};
 
 export const OrdersService = {
-  //   createOrder,
+  createOrder,
 };
